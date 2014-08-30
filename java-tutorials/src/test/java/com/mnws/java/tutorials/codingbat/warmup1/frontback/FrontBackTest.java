ce869fd36976d05f454b84aa5acac55b35279cca
cmcneill <cmcneill@bfs-product-57a.kana.com>
2014-08-30 13:46:08 +0100

    Adding description of test

--- java-tutorials/src/test/java/com/mnws/java/tutorials/codingbat/warmup1/frontback/FrontBackTest.java
+++ java-tutorials/src/test/java/com/mnws/java/tutorials/codingbat/warmup1/frontback/FrontBackTest.java
@@ -1,6 +1,8 @@
 /**
  * CodingBat Warmup Exercise: frontBack (http://codingbat.com/prob/p123384) 
  * 
+ * Given a string, return a new string where the first and last chars have been exchanged. 
+ * 
  * frontBack("code") -> "eodc"
  * frontBack("a") -> "a"
  * frontBack("ab") -> "ba"
